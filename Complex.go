a4c4ae18c311d52a387895017e95327ee825ce1d
TheTeaCat <joshua.r.osullivan@gmail.com>
2021-01-14 20:42:58 +0000

    Refactor

--- Complex.go
+++ Complex.go
@@ -17,46 +17,43 @@ func (c complex) absq() *big.Float {
 }
 
 func (c complex) abs() *big.Float {
-	return new(big.Float).Sqrt(
-		new(big.Float).Add(
-			new(big.Float).Mul(c.real, c.real),
-			new(big.Float).Mul(c.imag, c.imag)))
+	v := new(big.Float).Mul(c.real, c.real)
+	v.Add(v, new(big.Float).Mul(c.imag, c.imag))
+	return v.Sqrt(v)
 }
 
 func (c complex) mul(d complex) (e complex) {
-	e.real = new(big.Float).Sub(
-		new(big.Float).Mul(c.real, d.real),
-		new(big.Float).Mul(c.imag, d.imag))
-	e.imag = new(big.Float).Add(
-		new(big.Float).Mul(c.real, d.imag),
-		new(big.Float).Mul(c.imag, d.real))
+	//er = cr*dr - ci*di
+	e.real = new(big.Float).Mul(c.real, d.real)
+	e.real.Sub(e.real, new(big.Float).Mul(c.imag, d.imag))
+
+	//ei = cr*di + ci*dr
+	e.imag = new(big.Float).Mul(c.real, d.imag)
+	e.imag.Add(e.imag, new(big.Float).Mul(c.imag, d.real))
+
 	return e
 }
 
 func (c complex) div(d complex) (e complex) {
 	//Calculate the denom for both real and imag parts first...
-	denom := new(big.Float).Add(
-		new(big.Float).Mul(d.real, d.real),
-		new(big.Float).Mul(d.imag, d.imag))
+	denom := new(big.Float).Mul(d.real, d.real)
+	denom.Add(denom, new(big.Float).Mul(d.imag, d.imag))
 
 	//Real numerator (cr*dr + ci*di)
-	e.real.Quo(
-		new(big.Float).Add(
-			new(big.Float).Mul(c.real, d.real),
-			new(big.Float).Mul(c.imag, d.imag)),
-		denom)
+	e.real = new(big.Float).Mul(c.real, d.real)
+	e.real.Add(e.real, new(big.Float).Mul(c.imag, d.imag))
+	e.real.Quo(e.real, denom)
 
 	//Imag numerator (ci*dr - cr*di)
-	e.imag.Quo(
-		new(big.Float).Sub(
-			new(big.Float).Mul(c.imag, d.real),
-			new(big.Float).Mul(c.real, d.imag)),
-		denom)
+	e.imag = new(big.Float).Mul(c.imag, d.real)
+	e.imag.Sub(e.imag, new(big.Float).Mul(c.real, d.imag))
+	e.imag.Quo(e.imag, denom)
 
 	return e
 }
 
 func (c complex) add(d complex) (e complex) {
+	//trivial, er=cr+dr, ei=ci+di
 	e.real = new(big.Float).Add(c.real, d.real)
 	e.imag = new(big.Float).Add(c.imag, d.imag)
 	return e
